bee26ec00d92e94e8b807e4407a143ce0beca153
Neha Thorat <nehathorat431@gmail.com>
2026-01-26 00:24:48 +0530

    Add files via upload

--- Assignment15_1.py
+++ Assignment15_1.py
@@ -1,7 +1,7 @@
 #Write a lambda function using map () which accepts a list of numbers and returns a list of squares of each number.
 
 def main():
-    Data = [1, 2, 3, 4]
+    Data = [1, 2, 3, 4, 5]
     print("The data is:", Data)
 
     Mdata = list(map(lambda No: No * No, Data))
@@ -9,3 +9,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
