ac610fdb0f731e7742dffffd45f75072f12c89ad
Neha Thorat <nehathorat431@gmail.com>
2026-01-27 00:10:32 +0530

    Add files via upload

--- Assignment 20/Assignment20_5.py
+++ Assignment 20/Assignment20_5.py
@@ -7,46 +7,23 @@
 
 import threading
 
-def Prime(Value):
-    if Value == 1:
-        return False
-    
-    for i in range(2, Value):
-        if Value % i == 0:
-            return False
-    return True
+def PrintThread1():
+    for i in range(1, 51):
+        print(i)
 
-def NonPrime(Value):
-    if Value == 1:
-        return True
-    
-    for i in range(2, Value):
-        if Value % i == 0:
-            return True
-    return False
-
-def DisplayPrime(Data):
-    for i in range(len(Data)):
-        if Prime(Data[i]) == True:
-            print("Prime Numbers :", Data[i])
-
-
-def DisplayNonPrime(Data):
-    for i in range(len(Data)):
-        if NonPrime(Data[i]) == True:
-            print("NonPrime numbers :", Data[i])
+def PrintThread2():
+    for i in range(50, 0, -1):
+        print(i)
 
 def main():
-    Data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    Thread1 = threading.Thread(target=PrintThread1)
+    Thread2 = threading.Thread(target=PrintThread2)
+
+    Thread1.start()
+    Thread1.join()
     
-    Prime = threading.Thread(target=DisplayPrime, args=(Data,))
-    NonPrime = threading.Thread(target=DisplayNonPrime, args=(Data,))
-
-    Prime.start()
-    Prime.join()
-
-    NonPrime.start()
-    NonPrime.join()
+    Thread2.start()
+    Thread2.join()
 
 if __name__ == "__main__":
     main()
