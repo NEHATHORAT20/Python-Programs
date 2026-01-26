955e50a6dc6c41d15eac20835b5564e929153214
Neha Thorat <nehathorat431@gmail.com>
2026-01-27 00:11:31 +0530

    Add files via upload

--- Assignment 20/Assignment20_4.py
+++ Assignment 20/Assignment20_4.py
@@ -9,23 +9,53 @@
 
 import threading
 
-def PrintThread1():
-    for i in range(1, 51):
-        print(i)
+def LowerCase(Name):
+    Count = 0
+    for ch in Name:
+        if ch >= 'a' and ch <= 'z':
+            Count += 1
 
-def PrintThread2():
-    for i in range(50, 0, -1):
-        print(i)
+    print("Thread ID :", threading.get_ident())
+    print("Thread Name :", threading.current_thread().name)
+    print("Small characters :", Count)
+    print()
+
+def UpperCase(Name):
+    Count = 0
+    for ch in Name:
+        if ch >= 'A' and ch <= 'Z':
+            Count += 1
+
+    print("Thread ID :", threading.get_ident())
+    print("Thread Name :", threading.current_thread().name)
+    print("Capital characters :", Count)
+    print()
+
+def NumericDigit(Name):
+    Count = 0
+    for ch in Name:
+        if ch >= '0' and ch <= '9':
+            Count += 1
+
+    print("Thread ID :", threading.get_ident())
+    print("Thread Name :", threading.current_thread().name)
+    print("Digits :", Count)
+    print()
 
 def main():
-    Thread1 = threading.Thread(target=PrintThread1)
-    Thread2 = threading.Thread(target=PrintThread2)
+    Name = input("Enter string : ")
 
-    Thread1.start()
-    Thread1.join()
-    
-    Thread2.start()
-    Thread2.join()
+    Small = threading.Thread(target=LowerCase, args=(Name,), name="Small")
+    Capital = threading.Thread(target=UpperCase, args=(Name,), name="Capital")
+    Digits = threading.Thread(target=NumericDigit, args=(Name,), name="Digits")
+
+    Small.start()
+    Capital.start()
+    Digits.start()
+
+    Small.join()
+    Capital.join()
+    Digits.join()
 
 if __name__ == "__main__":
     main()
